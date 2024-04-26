c401b73686c57b05e9745bb588d8c774f369930a
NicowaxDelux <nycko111995@gmail.com>
2024-04-26 07:31:13 -0500

    funtion that shows the environmemt

---
+++ env.c
@@ -0,0 +1,15 @@
+#include "header.h"
+/**
+ * print_environment - Prints the current environment variables.
+ */
+void print_environment(void)
+{
+    extern char **environ;
+    char **env = environ;
+
+    while (*env != NULL)
+    {
+        printf("%s\n", *env);
+        env++;
+    }
+}
